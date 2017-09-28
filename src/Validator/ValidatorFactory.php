3a412b687e93f2b2765bbd16fe8edbb2f100bef5
Bram Van der Sype <bram.vandersype@gmail.com>
2017-09-28 13:53:46 +0200

    Created validator factory and moved test

---
+++ src/Validator/ValidatorFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Brammm\Vat\Validator;
+
+class ValidatorFactory
+{
+    public static function create()
+    {
+        return new Validator([
+            new ValidatorAT(),
+            new ValidatorBE(),
+            new ValidatorBG(),
+            new ValidatorCY(),
+            new ValidatorCZ(),
+            new ValidatorDE(),
+            new ValidatorDK(),
+            new ValidatorEE(),
+            new ValidatorEL(),
+            new ValidatorES(),
+            new ValidatorFI(),
+            new ValidatorFR(),
+            new ValidatorGB(),
+            new ValidatorHR(),
+            new ValidatorHU(),
+            new ValidatorIE(),
+            new ValidatorIT(),
+            new ValidatorLT(),
+            new ValidatorLU(),
+            new ValidatorLV(),
+            new ValidatorMT(),
+            new ValidatorNL(),
+            new ValidatorPL(),
+            new ValidatorPT(),
+            new ValidatorRO(),
+            new ValidatorSE(),
+            new ValidatorSI(),
+            new ValidatorSK(),
+        ]);
+    }
+}
